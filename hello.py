4f3516628095f9c21eb549b4328466f33187d107
Alice Yang <ayang@smith.edu>
2015-11-07 10:48:12 -0500

    fixed form post name

--- hello.py
+++ hello.py
@@ -8,23 +8,12 @@ def hello():
 
 @app.route("/post_find", methods=["POST"])
 def eventually():
-<<<<<<< Updated upstream
 	fbPageName = request.form.get("page_name")
-	if (getFacebookPageData(page_id) == 'page not found'):
-<<<<<<< HEAD
-		# do something
+	if (getFacebookPageData(fbPageName) == 'page not found'):
+		print 'page not found'
 	else:
 		return fbPageName
-=======
-		
-	return fbPageName
-=======
-	#fbPageName = request.form.get("page_name")
-	#getFacebookPageData(page_id)
-	#return fbPageName
-	return request.form.get("page_name")
->>>>>>> Stashed changes
->>>>>>> c84a2a0d943c32683a6ff3a15ad0684ff1ca69af
+
 
 
 if __name__ == "__main__":
