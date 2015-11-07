ec6cce627966139656be8caa2bdd2f4880a94084
faridasabry2 <apple@Barbarosa.local>
2015-11-07 10:44:46 -0500

    i don't know what i changed

--- hello.py
+++ hello.py
@@ -8,24 +8,13 @@ def hello():
 
 @app.route("/post_find", methods=["POST"])
 def eventually():
-<<<<<<< Updated upstream
 	fbPageName = request.form.get("page_name")
 	if (getFacebookPageData(page_id) == 'page not found'):
-<<<<<<< HEAD
 		# do something
 	else:
 		return fbPageName
-=======
-		
-	return fbPageName
-=======
-	#fbPageName = request.form.get("page_name")
-	#getFacebookPageData(page_id)
-	#return fbPageName
-	return request.form.get("page_name")
->>>>>>> Stashed changes
->>>>>>> c84a2a0d943c32683a6ff3a15ad0684ff1ca69af
 
+	return request.form.get("page_name")
 
 if __name__ == "__main__":
     app.run()
