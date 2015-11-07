c84a2a0d943c32683a6ff3a15ad0684ff1ca69af
faridasabry2 <apple@Barbarosa.local>
2015-11-07 10:38:43 -0500

    changes

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-import findSentiment
+#import findSentiment
 from flask import request, Flask, render_template
 app = Flask(__name__)
 
@@ -8,10 +8,17 @@ def hello():
 
 @app.route("/post_find", methods=["POST"])
 def eventually():
+<<<<<<< Updated upstream
 	fbPageName = request.form.get("page_name")
 	if (getFacebookPageData(page_id) == 'page not found'):
 		
 	return fbPageName
+=======
+	#fbPageName = request.form.get("page_name")
+	#getFacebookPageData(page_id)
+	#return fbPageName
+	return request.form.get("page_name")
+>>>>>>> Stashed changes
 
 
 if __name__ == "__main__":
